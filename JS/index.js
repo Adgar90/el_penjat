a4ff1c76e8fefdd2f00bdc8166f29f77c2f082c2
Adria <adriagp90@gmail.com>
2023-11-09 20:08:57 +0100

    afegint part grafica desenvolupada en classe 09/11

--- JS/index.js
+++ JS/index.js
@@ -4,47 +4,33 @@ let guanyades = 0;
 let perdudes = 0;
 
 
-function iniciaJoc() {
-    let opcio = prompt(`Escull una opció:
-    1. Iniciar un joc
-    2. Estadístiques
-    3. Sortir
-    `);
-    Number.isInteger(parseInt(opcio)) ? opcio = parseInt(opcio) : opcio = parseInt("0");
-    switch (opcio) {
-        case 1:
-            console.log("Comença el joc...");
-            novaPartida();
-            break;
-        case 2: // estadístiques
-            console.log(`Total partides: ${jugades}
-                - Guanyades (${guanyades == 0 && jugades == 0 ? "0" : ((guanyades/jugades)*100).toFixed(2)}%): ${guanyades}
-                - Perdudes (${perdudes == 0 && jugades == 0 ? "0" : ((perdudes/jugades)*100).toFixed(2)}%): ${perdudes}`
-            );
-            iniciaJoc();
-            break;
-        case 3:
-            console.log("Sortint...");
-            return;
-        default:
-            console.log("Opció no vàlida");
-            break;
-    }
-}
 
+let encertada = false
+let reset = document.getElementById("jocPenjat").innerHTML;
+
+function mostraStats() {
+    let statsWindow = window.open("", "", "width=400,height=400");
+    statsWindow.document.write(`Total partides: ${jugades}
+        - Guanyades (${guanyades == 0 && jugades == 0 ? "0" : ((guanyades/jugades)*100).toFixed(2)}%): ${guanyades}
+        - Perdudes (${perdudes == 0 && jugades == 0 ? "0" : ((perdudes/jugades)*100).toFixed(2)}%): ${perdudes}`
+    );
+}
 function novaPartida() {
-    jugades++; // suma 1 a les partides jugades;
-    let paraula = prompt("Introdueix una paraula").toUpperCase();
-    let longitud = paraula.length;
+    // variable oportunitats de joc
+    let posicioPenjat = 0;
+    // variable de control
+    // arrays de control
+    let arrEncriptat = []; // -> per marcar les posicions encriptades i canviar-les si la lletra es correcte
+    let arrAbsolut = []; // -> per indicar les posicions de les lletres i poder canviar l'estat en la paraula encriptada
+    let paraula = "";
     // reset de variables
     let paraulaCompleta = "";
     let paraulaControl = "";
     let lletresUtilitzades = "";
-    // arrays de control
-    let arrEncriptat = []; // -> per marcar les posicions encriptades i canviar-les si la lletra es correcte
-    let arrAbsolut = []; // -> per indicar les posicions de les lletres i poder canviar l'estat en la paraula encriptada
-    // variable oportunitats de joc
-    let videsUtilitzades = 0;
+    jugades++; // suma 1 a les partides jugades;
+    document.getElementById("abecedari").innerHTML = "";
+    paraula = prompt("Introdueix una paraula").toUpperCase();
+    let longitud = paraula.length;
     // definim la longitud de l'array amb la longitud de la paraula
     for (let i=0; i<longitud; i++) {
         arrAbsolut[i] = paraula[i];
@@ -52,70 +38,65 @@ function novaPartida() {
         paraulaCompleta += `${paraula[i]} `;
         paraulaControl += "_ ";
     }
-    while (true) {
-        console.log(paraulaControl);
-        console.log(`Lletres fallades ${videsUtilitzades}/6: ${lletresUtilitzades}`);
-        // si les oportunitats s'acaben suma 1 al contador de partides perdudes i surt
-        if (videsUtilitzades == 6) { 
-            perdudes++;
-            console.log("Mor penjat!");
-            break; 
-        }
-        let lletra = prompt("Introdueix una lletra").toUpperCase();
-        let incorrecte = true;
-        
-        if (lletraUtilitzada(lletresUtilitzades, lletra)) { 
-            console.log(`La lletra ${lletra} ja ha estat utilitzada.`);
-            videsUtilitzades++;
-            continue; 
-        }
-        // controlem que només passin 1 caracter i després validem amb la funció
-        if (lletra.length < 2 && esLletra(lletra)) {
-            lletresUtilitzades < 2 ? lletresUtilitzades += `${lletra}` : lletresUtilitzades += `, ${lletra}`;
-            
-            for (let i=0; i<arrAbsolut.length; i++) {
-                if (lletra == arrAbsolut[i]) { // si el valor de la lletra coincideix amb el valors d'algunes de les posicions, actualitzem l'array encriptat
-                    arrEncriptat[i] = arrAbsolut[i];
-                    incorrecte = false;
-                }
-            }
-            paraulaControl = retornaParaula(arrEncriptat);
-        } else {
-            // sortida d'error ja que no acceptem caràcters que no siguin lletres de l'abecedari
-            console.error("Oportunitat perduda. Has d'introduïr lletres de l'abecedari (a-z).");
-            videsUtilitzades++;
-            continue;
-        }
-        
-        // si la lletra no es correcte, l'usuari perd una vida
-        if (incorrecte) { videsUtilitzades++; }
-        
-        if (paraulaControl == paraulaCompleta) {
-            console.log(`Enhorabona, has guanyat! La paraula era: ${paraulaCompleta}`);
-            guanyades++;
-            break;
-        }
+    
+    document.getElementById("jocPenjat").innerHTML = reset;
+    // creem element p per mostrar la paraula
+    let paragraph = document.createElement("p");
+    paragraph.id = "paraula";
+    paragraph.textContent = paraulaControl;
+    document.getElementById("jocPenjat").appendChild(paragraph);
+
+
+    let abecedari = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (let i=0; i< abecedari.length; i++) {
+       let btn = document.createElement("button");
+       btn.textContent = abecedari[i];
+       btn.id = abecedari[i];
+       btn.addEventListener("click", function() {
+         let lletra = btn.innerHTML;
+         esLletraCorrecta(arrEncriptat, arrAbsolut, lletra);
+       });
+       document.getElementById("abecedari").appendChild(btn);
     }
-    iniciaJoc();
+    
+}
+// funcio per comprovar si existeix element
+function existeixElement(element) {
+    if (element.getElementsByTagName("p")) { return true; }
+    return false;
 }
 
-// funció per validar que forma part de l'abecedari i no és un caràcter diferent (ignore case)
-function esLletra(lletra) {
-    let lletres = /[a-z]/gi;
-    if (lletra.match(lletres)) { return true; }
-    return false;
+function esLletraCorrecta(arrEncriptat, arrAbsolut, lletra) { // TODO: editar nom de la funció
+    let paraulaTemporal = "";
+    for (let i=0; i<arrEncriptat.length; i++){
+        if (lletra == arrAbsolut[i]) {
+            arrEncriptat[i] = arrAbsolut[i];
+            paraulaTemporal+= `${arrAbsolut[i]} `;
+        } else {
+            paraulaTemporal+=`${arrEncriptat[i]} `;
+        }
+    }
+    lletraSetColor(arrAbsolut, lletra);
+    document.getElementById(lletra).disabled = true;
+    document.getElementById("paraula").textContent = paraulaTemporal;
+}
+// funcio per setejar el color del boto de la lletra
+function lletraSetColor(arrAbsolut, lletra) {
+    for (let i=0; i<arrAbsolut.length; i++) {
+        if (lletra == arrAbsolut[i]) { 
+            encertada = true;
+            break; 
+        }
+    }
+    if (encertada) {
+        document.getElementById(lletra).style.backgroundColor = "green";
+    } else {
+        document.getElementById(lletra).style.backgroundColor = "red";
+    }
 }
 function lletraUtilitzada(lletresUtilitzades, lletra) {
     for (let i=0; i<lletresUtilitzades.length; i++) {
         if (lletresUtilitzades[i] == lletra) { return true; }
     }
     return false;
-}
-// funció que reescriu la paraula a mostrar
-function retornaParaula(array) {
-    let x = "";
-    for (let i=0; i<array.length; i++) {
-        x += `${array[i]} `;
-    }
-    return x;
 }
