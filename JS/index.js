d88b8be9ea46df92958b86ffa8f5cdec175f7d04
Adria <adriagp90@gmail.com>
2023-11-13 20:01:27 +0100

    afegit actualitzacions del penjat (casi acabat)

--- JS/index.js
+++ JS/index.js
@@ -1,52 +1,56 @@
-// variables globas per contar les partides totals / guanyades / perdudes
-let jugades = 0;
-let guanyades = 0;
-let perdudes = 0;
-
-
-
+// variables globas
+let posicioPenjat = 0;
 let encertada = false
 let reset = document.getElementById("jocPenjat").innerHTML;
 
 function mostraStats() {
-    let statsWindow = window.open("", "", "width=400,height=400");
-    statsWindow.document.write(`Total partides: ${jugades}
-        - Guanyades (${guanyades == 0 && jugades == 0 ? "0" : ((guanyades/jugades)*100).toFixed(2)}%): ${guanyades}
+    let jugades = localStorage.getItem("jugades");
+    let guanyades = localStorage.getItem("guanyades");
+    let perdudes = localStorage.getItem("perdudes");
+    let statsWindow = window.open("", "", "width=400px", "height=400px");
+    statsWindow.document.write(`Total partides: ${jugades}<br>
+        - Guanyades (${guanyades == 0 && jugades == 0 ? "0" : ((guanyades/jugades)*100).toFixed(2)}%): ${guanyades}<br>
         - Perdudes (${perdudes == 0 && jugades == 0 ? "0" : ((perdudes/jugades)*100).toFixed(2)}%): ${perdudes}`
     );
 }
 function novaPartida() {
-    // variable oportunitats de joc
-    let posicioPenjat = 0;
-    // variable de control
+    posicioPenjat = 0;
     // arrays de control
-    let arrEncriptat = []; // -> per marcar les posicions encriptades i canviar-les si la lletra es correcte
-    let arrAbsolut = []; // -> per indicar les posicions de les lletres i poder canviar l'estat en la paraula encriptada
-    let paraula = "";
-    // reset de variables
-    let paraulaCompleta = "";
+    let encriptada = []; // -> per marcar les posicions encriptades i canviar-les si la lletra es correcte
+    let paraulaCorrecta = []; // -> per indicar les posicions de les lletres i poder canviar l'estat en la paraula encriptada
+    // reset de variables de control
     let paraulaControl = "";
-    let lletresUtilitzades = "";
-    jugades++; // suma 1 a les partides jugades;
+    
+    procesaDadesPartida("jugades"); // suma 1 a les partides jugades;
     document.getElementById("abecedari").innerHTML = "";
-    paraula = prompt("Introdueix una paraula").toUpperCase();
+    let paraula = prompt("Introdueix una paraula").toUpperCase();
+
+    // controlem nulls i empties
+    if (paraula == null || paraula == "") { return; }
+    
+
+    // omplim les nostres variables
     let longitud = paraula.length;
-    // definim la longitud de l'array amb la longitud de la paraula
     for (let i=0; i<longitud; i++) {
-        arrAbsolut[i] = paraula[i];
-        arrEncriptat[i] = "_";
-        paraulaCompleta += `${paraula[i]} `;
+        paraulaCorrecta[i] = paraula[i];
+        encriptada[i] = "_";
         paraulaControl += "_ ";
     }
-    
+
+    // fem reset de l'innerHTML del joc per evitar duplicats
     document.getElementById("jocPenjat").innerHTML = reset;
+
     // creem element p per mostrar la paraula
     let paragraph = document.createElement("p");
     paragraph.id = "paraula";
     paragraph.textContent = paraulaControl;
     document.getElementById("jocPenjat").appendChild(paragraph);
 
+    // setejem la posicio de la imatge del penjat
+    setPosicioPenjat(posicioPenjat);
+    posicioPenjat++;
 
+    // creem les botoneres de les lletres per poder jugar
     let abecedari = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for (let i=0; i< abecedari.length; i++) {
        let btn = document.createElement("button");
@@ -54,36 +58,46 @@ function novaPartida() {
        btn.id = abecedari[i];
        btn.addEventListener("click", function() {
          let lletra = btn.innerHTML;
-         esLletraCorrecta(arrEncriptat, arrAbsolut, lletra);
+         procesaParaula(encriptada, paraulaCorrecta, lletra);
+         procesaLletra(paraulaCorrecta, lletra);
+         sumaLletraUtilitzada(lletra);
        });
        document.getElementById("abecedari").appendChild(btn);
     }
     
 }
-// funcio per comprovar si existeix element
-function existeixElement(element) {
-    if (element.getElementsByTagName("p")) { return true; }
-    return false;
-}
 
-function esLletraCorrecta(arrEncriptat, arrAbsolut, lletra) { // TODO: editar nom de la funció
+function procesaParaula(encriptada, paraulaCorrecta, lletra) { // TODO: editar nom de la funció
+    // variable per guardar valors temporals
     let paraulaTemporal = "";
-    for (let i=0; i<arrEncriptat.length; i++){
-        if (lletra == arrAbsolut[i]) {
-            arrEncriptat[i] = arrAbsolut[i];
-            paraulaTemporal+= `${arrAbsolut[i]} `;
+    // for per recorrer la paraula encriptada i substituir si la lletra existeix en la paraula proposada
+    for (let i=0; i<encriptada.length; i++){
+        if (lletra == paraulaCorrecta[i]) {
+            encriptada[i] = paraulaCorrecta[i];
+            paraulaTemporal+= `${paraulaCorrecta[i]} `;
         } else {
-            paraulaTemporal+=`${arrEncriptat[i]} `;
+            paraulaTemporal+=`${encriptada[i]} `;
         }
     }
-    lletraSetColor(arrAbsolut, lletra);
+    // un cop usat el botó, el deshabilitem
     document.getElementById(lletra).disabled = true;
+    // actualitzem el valor del <p>
     document.getElementById("paraula").textContent = paraulaTemporal;
+    if (posicioPenjat == 6) {
+        alert("Has mort!");
+        disableButtons();
+        procesaDadesPartida("perdudes");
+    }
+    if (paraulaEncertada(encriptada, paraulaCorrecta)) {
+        alert("Has encertat la paraula");
+        disableButtons();
+        procesaDadesPartida("guanyades");
+    }
 }
-// funcio per setejar el color del boto de la lletra
-function lletraSetColor(arrAbsolut, lletra) {
-    for (let i=0; i<arrAbsolut.length; i++) {
-        if (lletra == arrAbsolut[i]) { 
+// funcio per procesar dades que manipulen la lletra (si es valida o no, la posicio del penjat, etc)
+function procesaLletra(paraulaCorrecta, lletra) {
+    for (let i=0; i<paraulaCorrecta.length; i++) {
+        if (lletra == paraulaCorrecta[i]) { 
             encertada = true;
             break; 
         }
@@ -92,11 +106,45 @@ function lletraSetColor(arrAbsolut, lletra) {
         document.getElementById(lletra).style.backgroundColor = "green";
     } else {
         document.getElementById(lletra).style.backgroundColor = "red";
+        setPosicioPenjat(posicioPenjat);
+        posicioPenjat++;
+    }
+    encertada = false;
+}
+// funcio per mostrar en quina posicio està el penjat
+function setPosicioPenjat(posicioPenjat) {
+    if (posicioPenjat < 7) { document.getElementById("imatgePenjat").src = `img/penjat_${posicioPenjat}.png`; }
+}
+// funcio per deshabilitar els butons un cop s'ha guanyat o perdut
+function disableButtons() {
+    let abecedari = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (let i=0; i< abecedari.length; i++) {
+       document.getElementById(abecedari[i]).disabled = true;
     }
 }
-function lletraUtilitzada(lletresUtilitzades, lletra) {
-    for (let i=0; i<lletresUtilitzades.length; i++) {
-        if (lletresUtilitzades[i] == lletra) { return true; }
+// funcio que comprova si l'usuari ha encertat la paraula
+function paraulaEncertada (encriptada, paraulaCorrecta) {
+    for (let i=0; i<encriptada.length; i++){
+        if (encriptada[i] != paraulaCorrecta[i]) {
+            return false;
+        }
     }
-    return false;
+    return true;
+}
+// procesa les dades de la partida
+function procesaDadesPartida(estatPartida) {
+    let partidaCount = localStorage.getItem(estatPartida);
+    partidaCount++;
+    localStorage.setItem(estatPartida, partidaCount);
+}
+// reseteja les stats de la partida
+function resetDadesPartida() {
+    localStorage.setItem("jugades", 0);
+    localStorage.setItem("guanyades", 0);
+    localStorage.setItem("perdudes", 0);
+}
+function sumaLletraUtilitzada(lletra) {
+    let lletresUtilitzades = document.getElementById("lletresUtilitzades").textContent;
+    lletresUtilitzades < 1 ? lletresUtilitzades += `Lletres utilitzades: ${lletra}` : lletresUtilitzades += `, ${lletra}`;
+    document.getElementById("lletresUtilitzades").textContent = lletresUtilitzades;
 }
